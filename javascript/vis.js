0238d19f42644d50e68ab7ed6f2da3d2292730b2
Emily Youngward <eyoungwa@gmail.com>
2025-10-01 19:25:27 -0700

    deleting unnecessary code

--- javascript/vis.js
+++ javascript/vis.js
@@ -112,13 +112,3 @@ function animate() {
     requestAnimationFrame(animate);
 }
 animate();
-
-
-
-// Example: Draw a simple circle in the SVG
-// const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-// circle.setAttribute('cx', '400');
-// circle.setAttribute('cy', '250');
-// circle.setAttribute('r', '50');
-// circle.setAttribute('fill', 'blue');
-// svg.appendChild(circle);
